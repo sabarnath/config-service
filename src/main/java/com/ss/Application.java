261e7ca7c29a1f9fb6e4af98e18f22bfa10acb87
sabarinathan soundararajan <ssoundararajan@ghx.com>
2017-02-02 11:08:54 +0530

    initial commits

---
+++ src/main/java/com/ss/Application.java
@@ -0,0 +1,16 @@
+package com.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
