838e9103da4e1ad163d17900428b17cebba5fa6d
Sabarinathan Soundararajan <ssoundararajan@ghx.com>
2019-08-22 10:50:29 +0530

    EGX-1234: test

--- src/main/java/com/ss/Application.java
+++ src/main/java/com/ss/Application.java
@@ -1,11 +1,11 @@
 package com.ss;
 
-import org.springframework.boot.SpringApplication;
+/*import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;*/
 
-@Configuration
+//@Configuration
 @EnableConfigServer
 @SpringBootApplication
 public class Application {
